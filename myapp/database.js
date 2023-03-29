bec2a6da37c9eb3318eb02ddbc06335af508e5e7
azimusshan <azimusshankhanazi@gmail.com>
2023-03-29 15:56:44 +0530

    Update

--- myapp/database.js
+++ myapp/database.js
@@ -7,21 +7,5 @@ let data = [
         "numbers":["1","334","4"],
         "alphabets":["A","R"]
     },
-    // {
-    //     "is_success": true,
-    //     "user_id":"jphn_doe_17091999",
-    //     "email":"john@xyz.com",
-    //     "roll_number":"ABCD123",
-    //     "numbers":["2","4","5","92"],
-    //     "alphabets":[]
-    // },
-    // {
-    //     "is_success": true,
-    //     "user_id":"jphn_doe_17091999",
-    //     "email":"john@xyz.com",
-    //     "roll_number":"ABCD123",
-    //     "numbers":[],
-    //     "alphabets":["A","ABCD","DOE"]
-    // }
 ];
 module.exports=data;
